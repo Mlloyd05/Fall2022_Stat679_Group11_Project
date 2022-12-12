8e43f566eeed37c9d648931a336ca41547cbc9ca
Marwan Lloyd <Marwan.Lloyd@wisc.edu>
2022-12-11 22:40:08 -0600

    Lightning talks set up

--- Vis_Hosting/General_Olympics/server.R
+++ Vis_Hosting/General_Olympics/server.R
@@ -1,26 +1,12 @@
-olympics <- read_csv("Shiny_Data/athlete_events.csv", col_types = cols(
-  ID = col_character(),
-  Name = col_character(),
-  Sex = col_factor(levels = c("M","F")),
-  Age =  col_integer(),
-  Height = col_double(),
-  Weight = col_double(),
-  Team = col_character(),
-  NOC = col_character(),
-  Games = col_character(),
-  Year = col_integer(),
-  Season = col_factor(levels = c("Summer","Winter")),
-  City = col_character(),
-  Sport = col_character(),
-  Event = col_character(),
-  Medal = col_factor(levels = c("Gold","Silver","Bronze"))
-)
-)
+library(shiny)
+library(shinythemes)
+library(rsconnect)
+library(tidyverse)
+library(ggplot2)
+library(ggthemes)
+theme_set(theme_gray())
 
-olympics <- olympics[!is.na(olympics$Age),]
-olympics <- olympics[!is.na(olympics$Sex),]
-olympics <- olympics[!is.na(olympics$Year),]
-glimpse(olympics)
+olympics <- read_csv("Shiny_Data/athlete_events.csv")
 
 ### functions used in app
 line_plot <- function(df) {
@@ -29,18 +15,86 @@ line_plot <- function(df) {
     ylab("Number of Athletes")
 }
 
+agged_sport_data <- cbind(aggregate(Age ~ Sport, olympics, mean), aggregate(ID ~ Sport, olympics, length)$ID, aggregate(Event ~ Sport, olympics, function(Event) length(unique(Event)))$Event)
+colnames(agged_sport_data) = c("Sport", "Age", "Count", "Unique_Events")
+agged_sport_data$Athletes_Per_Event <- agged_sport_data$Count / agged_sport_data$Unique_Events
+agged_sport_data <- agged_sport_data[order(agged_sport_data$Age), ]  # sort
+agged_sport_data$Sport <- factor(agged_sport_data$Sport, levels = agged_sport_data$Sport)
+col = colnames(olympics)
+pot = c(col[4:6],col[10])
+
 server <- function(input, output) {
+  #The below code renders the plot and connects with the ui object, based on the sports selected it produces various color coded dot graphs, colored by sport.
+  output$olympics_plot <- renderPlot ({
+    ggplot(aggregate(Medal ~ Age + Sport, olympics[(!is.na(olympics$Medal) &
+                                                      (olympics$Sport %in% input$Sport_choice)), ], length)) + geom_point(aes(x = Age, y = Medal, col = Sport))
+  })
+  #The below code adds labels to the graph.
+  brks <- seq(-200, 200, 50)
+  lbls = paste0(as.character(c(seq(200, 0,-50), seq(50, 200, 50))))
+  
+  #This is a gender based plot which plots bars in either direction based on gender and fills color based on gender based on the sport selected.
+  output$Gender_olympics_plot <- renderPlot ({
+    ggplot(
+      aggregate(Medal ~ Age + Sport + Sex, olympics[(!is.na(olympics$Medal) &
+                                                       (olympics$Sport %in% input$Gender_Sport_choice)), ], length),
+      aes(
+        x = Age,
+        y = ifelse(Sex == "M", Medal * -1, Medal),
+        fill = Sex
+      )
+    ) +
+      geom_bar(stat = "identity", width = .6) +   # draw the bars
+      scale_y_continuous(breaks = brks,   # Breaks
+                         labels = lbls) + # Labels
+      coord_flip() +  # Flip axes
+      labs(title = "Comparing Gender Data", x = "Medals", y = "Age" ) +
+      theme(plot.title = element_text(hjust = .5),
+            axis.ticks = element_blank()) +   # Centre plot title
+      scale_fill_brewer(palette = "Dark2")  # Color palette
+    
+  })
+  oly_subset <- reactive({
+    olympics %>%
+      mutate(selected = 1 * (Team %in% input$team_choice) * (Sport %in% input$sport_choice)) %>%
+      filter(selected == 1)
+  })
+  
+  #Plotting based on the aes_string function that allows the selection of x and y axes by the user for more customizability.
+  output$Effectiveness <- renderPlot({
+    ggplot(oly_subset()) +
+      geom_jitter(
+        aes_string(
+          input$Metric_choice,
+          input$Metric_choice2,
+          color = "Medal",
+          shape = "Sex"
+        ),
+        width = .1,
+        height = .1
+      )
+  })
+  #Renders a table of the data points that are a part of the subset.
+  output$Table <- renderTable(oly_subset())
+  
+  #Age distribution plots
   olympics_subset <- reactive({
     olympics %>%
       mutate(selected = (
-        #(Pos %in% input$pos) &
         (Sport %in% input$Sport) &
           (Year >= input$Year[1]) &
           (Year <= input$Year[2])
       ))
   })
-  
-  output$line <- renderPlot({
+  output$olympics_age_dist_plot <- renderPlot({
     line_plot(olympics[olympics$Sport %in% input$Sport & olympics$Year >= input$Year[1] & olympics$Year <= input$Year[2],])
   })
+  
+  output$athlete_distribution <- renderPlot({ggplot(agged_sport_data, aes(x=Sport, y=Athletes_Per_Event, fill = Age)) + 
+      geom_bar(stat="identity", width=.5) + 
+      labs(title="Ordered Bar Chart", 
+           subtitle="Avg Age Vs Athlete Per Event Count", 
+           caption="source: Olympics Dataset") + 
+      theme(axis.text.x = element_text(angle=90, vjust=0.6)) + scale_fill_gradient2(low = "red", mid = "orange", high = "blue", midpoint = 12) + labs(y = "Athletes per Event")})
+  
 }
--- Vis_Hosting/General_Olympics/ui.R
+++ Vis_Hosting/General_Olympics/ui.R
@@ -3,30 +3,10 @@ library(shinythemes)
 library(rsconnect)
 library(tidyverse)
 library(ggplot2)
+library(ggthemes)
+theme_set(theme_gray())
 
-olympics <- read_csv("Shiny_Data/athlete_events.csv", col_types = cols(
-  ID = col_character(),
-  Name = col_character(),
-  Sex = col_factor(levels = c("M","F")),
-  Age =  col_integer(),
-  Height = col_double(),
-  Weight = col_double(),
-  Team = col_character(),
-  NOC = col_character(),
-  Games = col_character(),
-  Year = col_integer(),
-  Season = col_factor(levels = c("Summer","Winter")),
-  City = col_character(),
-  Sport = col_character(),
-  Event = col_character(),
-  Medal = col_factor(levels = c("Gold","Silver","Bronze"))
-)
-)
-
-olympics <- olympics[!is.na(olympics$Age),]
-olympics <- olympics[!is.na(olympics$Sex),]
-olympics <- olympics[!is.na(olympics$Year),]
-glimpse(olympics)
+olympics <- read_csv("Shiny_Data/athlete_events.csv")
 
 ### functions used in app
 line_plot <- function(df) {
@@ -35,13 +15,69 @@ line_plot <- function(df) {
     ylab("Number of Athletes")
 }
 
+agged_sport_data <- cbind(aggregate(Age ~ Sport, olympics, mean), aggregate(ID ~ Sport, olympics, length)$ID, aggregate(Event ~ Sport, olympics, function(Event) length(unique(Event)))$Event)
+colnames(agged_sport_data) = c("Sport", "Age", "Count", "Unique_Events")
+agged_sport_data$Athletes_Per_Event <- agged_sport_data$Count / agged_sport_data$Unique_Events
+agged_sport_data <- agged_sport_data[order(agged_sport_data$Age), ]  # sort
+agged_sport_data$Sport <- factor(agged_sport_data$Sport, levels = agged_sport_data$Sport)
+col = colnames(olympics)
+pot = c(col[4:6],col[10])
+
 
 ### definition of app
-ui <- fluidPage(
-  titlePanel("Olympic Athletes Age Visualization"),
-  selectInput("Sport", label = "Sport", choices = unique(olympics$Sport), multiple = TRUE),
-  #pickerInput("Sport", "Sport", Sport, options = list(`actions-box` = TRUE),multiple = T),
-  #pickerInput("pos", "Position", pos, options = list(`actions-box` = TRUE),multiple = T),
-  sliderInput("Year", "Year", min = min(olympics$Year), max = max(olympics$Year), c(2000, 2005), sep = ""),
-  plotOutput("line")
-)
+ui <-
+  navbarPage(
+    "Group 11 Shiny Prototype",
+    collapsible = TRUE,
+    inverse = TRUE,
+    theme = shinytheme("spacelab"),
+    tabPanel(
+      "Olympic Sports - Age vs. Medal",
+      sidebarLayout(sidebarPanel(
+        selectInput("Sport_choice", label = "Sport", choices = unique(olympics$Sport), multiple = TRUE, selected = c('Basketball', 'Football') ) ),
+        mainPanel(plotOutput("olympics_plot")),)
+    ),
+    tabPanel(
+      "Sport-Gender Breakdown by Age",
+      sidebarLayout(sidebarPanel(
+        selectInput("Gender_Sport_choice", label = "Sport", choices = unique(olympics$Sport), multiple = TRUE, selected = 'Basketball')
+      ),
+      mainPanel(plotOutput("Gender_olympics_plot")),)
+    ),
+    tabPanel(
+      "Olympics Athelete Age Distribution",
+      sidebarLayout(
+        sidebarPanel(
+          style = paste0("height: 27vh; overflow-y: auto;"),
+          selectInput("Sport", label = "Sport", choices = unique(olympics$Sport), 
+                      multiple = TRUE, selected = c('Basketball', 'Football') ),
+          sliderInput("Year", "Year", min = min(olympics$Year), max = max(olympics$Year), c(1980, 2010), sep = "" ),
+          plotOutput("line")
+        ),
+        mainPanel(
+          plotOutput("olympics_age_dist_plot")
+        ),
+      )
+      
+    ),
+    tabPanel(
+      "Athlete Analysis",
+      fluidRow(column(
+        4,
+        selectInput("sport_choice", label = "Sport", choices = unique(olympics$Sport), multiple = TRUE , selected = 'Basketball') ),
+        column(
+          4,
+          selectInput("team_choice", label = "Team", choices = unique(olympics$Team), multiple = TRUE, selected = 'United States')
+        )),
+      selectInput("Metric_choice", label = "PlayerMetricX", choices = pot, selected = 'Age'),
+      selectInput("Metric_choice2", label = "PlayerMetricY", choices = pot, selected = 'Height'),
+      plotOutput("Effectiveness"),
+      tableOutput("Table")
+    ),
+    tabPanel(
+      "Age vs. Athlete Count/Event",
+      plotOutput("athlete_distribution")
+    )
+    
+    
+  )
